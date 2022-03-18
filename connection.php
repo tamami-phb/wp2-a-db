d3e0a91e776385beed35ad3e61c0350855d61ccc
tamami <tamami.oka@gmail.com>
2022-03-18 16:23:20 +0700

    update db connection

--- connection.php
+++ connection.php
@@ -1,5 +1,7 @@
 <?php
-$conn_string = "host=172.28.0.2 port=5432 dbname=wp2_a user=postgres password=rahasia";
+$conn_string = "host=satao.db.elephantsql.com port=5432 " . 
+    "dbname=fzxmwwps user=fzxmwwps " .
+    "password=UOgfEogE3J3qr3CdC447Gep8iQBfacZI";
 $conn = pg_connect($conn_string);
 
 if($conn) {
